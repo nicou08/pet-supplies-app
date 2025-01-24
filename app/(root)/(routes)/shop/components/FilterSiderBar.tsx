2532db8e56edca73a19eae1c7c618ba0ccd994cf
Nicolas Urrego Guasca <nicolas.guasca16@gmail.com>
2025-01-24 10:48:57 -0700

    Changed main filters

--- app/(root)/(routes)/shop/components/FilterSiderBar.tsx
+++ app/(root)/(routes)/shop/components/FilterSiderBar.tsx
@@ -4,122 +4,114 @@ import { useState } from "react";
 
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Switch } from "@/components/ui/switch";
 import { Slider } from "@/components/ui/slider";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const items = [
-  {
-    id: "food",
-    label: "Food",
-  },
-  {
-    id: "toys",
-    label: "Toys",
-  },
-  {
-    id: "cages",
-    label: "Cages",
-  },
-  {
-    id: "fish-tanks",
-    label: "Fish Tanks",
-  },
+  { id: "food", label: "Food" },
+  { id: "toys", label: "Toys" },
+  { id: "cages", label: "Cages" },
+  { id: "fish-tanks", label: "Fish Tanks" },
 ];
 
 const pets = [
-  {
-    id: "dogs",
-    label: "Dogs",
-  },
-  {
-    id: "cats",
-    label: "Cats",
-  },
-  {
-    id: "guinea-pigs",
-    label: "Guinea Pigs",
-  },
-  {
-    id: "rabbits",
-    label: "Rabbits",
-  },
-  {
-    id: "birds",
-    label: "Birds",
-  },
-  {
-    id: "fish",
-    label: "Fish",
-  },
-  {
-    id: "exotic-pets",
-    label: "Exotic Pets",
-  },
+  { id: "dogs", label: "Dogs" },
+  { id: "cats", label: "Cats" },
+  { id: "guinea-pigs", label: "Guinea Pigs" },
+  { id: "rabbits", label: "Rabbits" },
+  { id: "birds", label: "Birds" },
+  { id: "fish", label: "Fish" },
+  { id: "exotic-pets", label: "Exotic Pets" },
+];
+
+const offers = [
+  { id: "on-sale", label: "On Sale" },
+  { id: "new-arrivals", label: "New Arrivals" },
+  { id: "clearance", label: "Clearance" },
 ];
 
 export function FilterSideBar() {
+  // Price Range
   const [priceRange, setPriceRange] = useState([0, 100]);
-
   const handlePriceChange = (value: number[]) => {
     setPriceRange(value);
   };
 
   return (
     <div className="col-span-1 md:col-span-1 ">
-      <div className="text-xl font-bold">Type</div>
-
-      {/* Products */}
-      <div className="flex items-center pt-3 space-x-2">
-        <Checkbox id="all-types" />
+      {/* Status */}
+      <div className="flex items-center pt-0 pb-3 space-x-3">
+        <Switch id="all-types" />
         <Label htmlFor="all-types" className="text-md font-medium">
-          All types
+          In-Stock
         </Label>
       </div>
-      {items.map((item) => (
-        <div key={item.id} className="flex items-center pt-3 space-x-2">
-          <Checkbox key={item.id} id={item.id} />
-          <Label htmlFor={item.id} className="text-md font-medium">
-            {item.label}
+
+      {/* Offers */}
+      <div className="py-3">
+        <div className="text-xl font-bold">Current Offers</div>
+        {offers.map((item) => (
+          <div key={item.id} className="flex items-center pt-3 space-x-2">
+            <Checkbox key={item.id} id={item.id} />
+            <Label htmlFor={item.id} className="text-md font-medium">
+              {item.label}
+            </Label>
+          </div>
+        ))}
+      </div>
+
+      {/* Products Type */}
+      <div className="py-3">
+        <div className="text-xl font-bold">Shop by Types</div>
+        <div className="flex items-center pt-3 space-x-2">
+          <Checkbox id="all-types" />
+          <Label htmlFor="all-types" className="text-md font-medium">
+            All types
           </Label>
         </div>
-      ))}
+        {items.map((item) => (
+          <div key={item.id} className="flex items-center pt-3 space-x-2">
+            <Checkbox key={item.id} id={item.id} />
+            <Label htmlFor={item.id} className="text-md font-medium">
+              {item.label}
+            </Label>
+          </div>
+        ))}
+      </div>
 
       {/* Pets */}
-      <div className="text-xl font-bold pt-5">Pets</div>
-
-      <div className="flex items-center pt-3 space-x-2">
-        <Checkbox id="all-types" />
-        <Label htmlFor="all-types" className="text-md font-medium">
-          All Pets
-        </Label>
-      </div>
-      {pets.map((item) => (
-        <div key={item.id} className="flex items-center pt-3 space-x-2">
-          <Checkbox key={item.id} id={item.id} />
-          <Label htmlFor={item.id} className="text-md font-medium">
-            {item.label}
-          </Label>
+      <div className="py-3">
+        <div className="text-xl font-bold">Pets</div>
+        <div className="grid grid-cols-2">
+          <div className="flex items-center pt-3 space-x-2">
+            <Checkbox id="all-types" />
+            <Label htmlFor="all-types" className="text-md font-medium">
+              All Pets
+            </Label>
+          </div>
+          {pets.map((item) => (
+            <div key={item.id} className="flex items-center pt-3 space-x-2">
+              <Checkbox key={item.id} id={item.id} />
+              <Label htmlFor={item.id} className="text-md font-medium">
+                {item.label}
+              </Label>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
 
       {/* Price Range */}
-      <div className="mt-4">
+      <div className="py-3">
         <div className="text-lg font-semibold">Price Range</div>
-        {/* <Slider
-          min={0}
-          max={500}
-          step={10}
-          value={priceRange}
-          onValueChange={handlePriceChange}
-        /> */}
         <Slider
           id="price-range"
           min={0}
           max={100}
-          step={3}
+          step={5}
           value={priceRange}
-          onValueChange={setPriceRange}
-          className="mt-2"
+          onValueChange={handlePriceChange}
+          className="pt-6"
+          minStepsBetweenThumbs={1}
         />
         <div className="flex justify-between text-sm mt-2">
           <span>${priceRange[0]}</span>
