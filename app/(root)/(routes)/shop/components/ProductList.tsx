e9b7ce56d2f6b8a140965416ce119cb5656845a5
Nicolas Urrego Guasca <nicolas.guasca16@gmail.com>
2025-01-26 22:01:16 -0700

    Added Dropdown button for product listing ordering

--- app/(root)/(routes)/shop/components/ProductList.tsx
+++ app/(root)/(routes)/shop/components/ProductList.tsx
@@ -1,4 +1,15 @@
+import { ChevronDown } from "lucide-react";
+
 import { ProductCard } from "./ProductCard";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 const dummyProducts = [
   {
@@ -134,6 +145,21 @@ const dummyProducts = [
 export function ProductList() {
   return (
     <div className="col-span-3 w-full grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="col-span-4 flex justify-end">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Best Match <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>Best Match</DropdownMenuItem>
+            <DropdownMenuItem>Price Low-High</DropdownMenuItem>
+            <DropdownMenuItem>Price High-Low</DropdownMenuItem>
+            <DropdownMenuItem>Highest Rated</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
       {dummyProducts.map((product, index) => (
         <ProductCard key={index} product={product} />
       ))}
