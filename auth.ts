c7dd4f5fec7a767f4fd08b10ad5375d22c0e8ff4
Nicolas Urrego Guasca <nicolas.guasca16@gmail.com>
2025-02-07 17:26:58 -0700

    Cleaned up authenticaiton files

--- auth.ts
+++ auth.ts
@@ -18,59 +18,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     signIn: "/sign-in",
   },
   callbacks: {
-    // authorized({ request, auth }) {
-    //   const { pathname } = request.nextUrl
-    //   if (pathname === "/middleware-example") return !!auth
-    //   return true
-    // },
     jwt({ token, trigger, session }) {
       if (trigger === "update") token.name = session?.user?.name;
       return token;
     },
   },
-  // callbacks: {
-  //   // signIn: async () => {
-  //   //   console.log("Sign in callback");
-  //   //   return true;
-  //   // }
-  //   async signIn({ user }) {
-  //     /**
-  //      * Check if user is in the database
-  //      */
-  //     console.log("Sign in callback");
-
-  //     // Checking if user is valid
-  //     // if (!user || !user.email) {
-  //     //   return false;
-  //     //   // throw new Error("Invalid email");
-  //     // }
-
-  //     // const { email } = user;
-
-  //     // const existingUser = await prisma.user.findUnique({
-  //     //   where: {
-  //     //     email: email,
-  //     //   },
-  //     // });
-
-  //     // if (!existingUser) {
-  //     //   console.log("Callback: User not found in database");
-
-  //     //   if (!user.name || !user.image) {
-  //     //     return false;
-  //     //   }
-
-  //     //   const { name, image } = user;
-  //     //   await prisma.user.create({
-  //     //     data: {
-  //     //       email: email,
-  //     //       name: name,
-  //     //       image: image,
-  //     //     },
-  //     //   });
-  //     // }
-
-  //     return true;
-  //   },
-  // },
 });
